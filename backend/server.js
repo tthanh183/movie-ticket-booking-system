74bd121261ec1aa3a4517ebfe913004f201fa5f2
tthanh183 <trantanthanh29@gmail.com>
2024-10-15 21:00:01 +0700

    project init

---
+++ backend/server.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import path from 'path';
+
+import { connectDB } from './lib/db.js';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: '10mb' }));
+app.use(cookieParser);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+}
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+  connectDB();
+});
+
+
