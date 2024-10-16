e6063f0c5bd1f3078a3758b02503f574519a54ce
tthanh183 <trantanthanh29@gmail.com>
2024-10-16 21:09:56 +0700

    refractor code

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,59 +19,64 @@ const Header = () => {
           Silver Screen
         </Typography>
         <div className="hidden md:flex space-x-6">
-          <Typography
-            as="a"
-            href="/"
-            variant="small"
-            color="blue-gray"
-            className="font-medium text-black hover:text-gray-400"
-          >
-            <ListItem className="flex items-center gap-2 py-2 pr-4">
-              Home
-            </ListItem>
-          </Typography>
-          <Typography
-            as="a"
-            href="#"
-            variant="small"
-            color="blue-gray"
-            className="font-medium text-black hover:text-gray-400"
-          >
-            <ListItem className="flex items-center gap-2 py-2 pr-4">
-              About
-            </ListItem>
-          </Typography>
-          <Typography
-            as="a"
-            href="#"
-            variant="small"
-            color="blue-gray"
-            className="font-medium text-black hover:text-gray-400"
-          >
-            <ListItem className="flex items-center gap-2 py-2 pr-4">
-              Contact Us
-            </ListItem>
-          </Typography>
-          {isAdmin && (
-            <Link
-              to="/dashboard"
-              className="flex items-center gap-2 py-2 pr-4 text-white hover:text-gray-400"
+          <Link to="/">
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="font-medium text-black hover:text-gray-400"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
+              <ListItem className="flex items-center gap-2 py-2 pr-4">
+                Home
+              </ListItem>
+            </Typography>
+          </Link>
+          <Link to="#">
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="font-medium text-black hover:text-gray-400"
+            >
+              <ListItem className="flex items-center gap-2 py-2 pr-4">
+                About
+              </ListItem>
+            </Typography>
+          </Link>
+          <Link to="#">
+            <Typography
+              variant="small"
+              color="blue-gray"
+              className="font-medium text-black hover:text-gray-400"
+            >
+              <ListItem className="flex items-center gap-2 py-2 pr-4">
+                Contact Us
+              </ListItem>
+            </Typography>
+          </Link>
+          {isAdmin && (
+            <Link to="/admin/dashboard">
+              <Typography
+                variant="small"
+                color="blue-gray"
+                className="font-medium text-black hover:text-gray-400"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
-                />
-              </svg>
-              Dashboard
+                <ListItem className="flex items-center gap-2 py-2 pr-4">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="size-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                    />
+                  </svg>
+                  Dashboard
+                </ListItem>
+              </Typography>
             </Link>
           )}
         </div>
@@ -79,28 +84,29 @@ const Header = () => {
           {user ? (
             <>
               <Typography
-                as="li"
+                as="a"
+                href="#"
                 variant="small"
                 color="blue-gray"
-                className="flex items-center gap-x-2 p-1 font-medium"
+                className="flex items-center gap-x-2 p-1 font-medium text-black hover:text-gray-400"
               >
-                <svg
-                  width="16"
-                  height="17"
-                  viewBox="0 0 16 17"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M16 8.5C16 10.6217 15.1571 12.6566 13.6569 14.1569C12.1566 15.6571 10.1217 16.5 8 16.5C5.87827 16.5 3.84344 15.6571 2.34315 14.1569C0.842855 12.6566 0 10.6217 0 8.5C0 6.37827 0.842855 4.34344 2.34315 2.84315C3.84344 1.34285 5.87827 0.5 8 0.5C10.1217 0.5 12.1566 1.34285 13.6569 2.84315C15.1571 4.34344 16 6.37827 16 8.5ZM10 5.5C10 6.03043 9.78929 6.53914 9.41421 6.91421C9.03914 7.28929 8.53043 7.5 8 7.5C7.46957 7.5 6.96086 7.28929 6.58579 6.91421C6.21071 6.53914 6 6.03043 6 5.5C6 4.96957 6.21071 4.46086 6.58579 4.08579C6.96086 3.71071 7.46957 3.5 8 3.5C8.53043 3.5 9.03914 3.71071 9.41421 4.08579C9.78929 4.46086 10 4.96957 10 5.5ZM8 9.5C7.0426 9.49981 6.10528 9.77449 5.29942 10.2914C4.49356 10.8083 3.85304 11.5457 3.454 12.416C4.01668 13.0706 4.71427 13.5958 5.49894 13.9555C6.28362 14.3152 7.13681 14.5009 8 14.5C8.86319 14.5009 9.71638 14.3152 10.5011 13.9555C11.2857 13.5958 11.9833 13.0706 12.546 12.416C12.147 11.5457 11.5064 10.8083 10.7006 10.2914C9.89472 9.77449 8.9574 9.49981 8 9.5Z"
-                    fill="#90A4AE"
-                  />
-                </svg>
-                <a href="#" className="flex items-center">
+                <ListItem className="flex items-center gap-2 py-2 pr-1">
+                  <svg
+                    width="16"
+                    height="17"
+                    viewBox="0 0 16 17"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      clipRule="evenodd"
+                      d="M16 8.5C16 10.6217 15.1571 12.6566 13.6569 14.1569C12.1566 15.6571 10.1217 16.5 8 16.5C5.87827 16.5 3.84344 15.6571 2.34315 14.1569C0.842855 12.6566 0 10.6217 0 8.5C0 6.37827 0.842855 4.34344 2.34315 2.84315C3.84344 1.34285 5.87827 0.5 8 0.5C10.1217 0.5 12.1566 1.34285 13.6569 2.84315C15.1571 4.34344 16 6.37827 16 8.5ZM10 5.5C10 6.03043 9.78929 6.53914 9.41421 6.91421C9.03914 7.28929 8.53043 7.5 8 7.5C7.46957 7.5 6.96086 7.28929 6.58579 6.91421C6.21071 6.53914 6 6.03043 6 5.5C6 4.96957 6.21071 4.46086 6.58579 4.08579C6.96086 3.71071 7.46957 3.5 8 3.5C8.53043 3.5 9.03914 3.71071 9.41421 4.08579C9.78929 4.46086 10 4.96957 10 5.5ZM8 9.5C7.0426 9.49981 6.10528 9.77449 5.29942 10.2914C4.49356 10.8083 3.85304 11.5457 3.454 12.416C4.01668 13.0706 4.71427 13.5958 5.49894 13.9555C6.28362 14.3152 7.13681 14.5009 8 14.5C8.86319 14.5009 9.71638 14.3152 10.5011 13.9555C11.2857 13.5958 11.9833 13.0706 12.546 12.416C12.147 11.5457 11.5064 10.8083 10.7006 10.2914C9.89472 9.77449 8.9574 9.49981 8 9.5Z"
+                      fill="#90A4AE"
+                    />
+                  </svg>
                   Account
-                </a>
+                </ListItem>
               </Typography>
               <Button
                 variant="text"
