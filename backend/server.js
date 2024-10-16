bba9bc04d39c778221d8966b52b074410cbe50e9
tthanh183 <trantanthanh29@gmail.com>
2024-10-16 18:17:59 +0700

    add cors config

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 import path from 'path';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.route.js';
 import cinemaRoutes from './routes/cinema.route.js';
@@ -14,11 +15,17 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
 const __dirname = path.resolve();
 
 app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/cinemas', cinemaRoutes);
