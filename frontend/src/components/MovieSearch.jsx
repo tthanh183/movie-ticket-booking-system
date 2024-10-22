bca8a61778c3ae03d1f577ce034447207a702249
tthanh183 <trantanthanh29@gmail.com>
2024-10-22 20:25:17 +0700

    refractor code

--- frontend/src/components/MovieSearch.jsx
+++ frontend/src/components/MovieSearch.jsx
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
+import { FaSearch } from 'react-icons/fa'; 
 
 const MovieSearch = ({ searchTerm, onSearch }) => {
   return (
-    <div className="mb-4">
+    <div className="mb-4 relative">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <FaSearch className="text-gray-400" />
+      </div>
       <input
         type="text"
         placeholder="Search movies..."
         value={searchTerm}
         onChange={e => onSearch(e.target.value)}
-        className="border p-2 rounded w-full"
+        className="border rounded-md pl-10 p-2 w-full" 
       />
     </div>
   );
